4ef7fae4228439a9e165742249bcc91b8f02aabe
Xinyuan Yang <yangxinyuan100@gmail.com>
2020-12-03 08:49:30 +0800

    Add FileOperation

---
+++ src/main/java/com/cs585/FileOperation.java
@@ -0,0 +1,60 @@
+package com.cs585;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IOUtils;
+
+public class FileOperation {
+    // initialize this class first:     FileOperation fo = new FileOperation();
+    // use the function:  eg:           fo.deleteData("hdfs://localhost:9000", "/user/output");
+
+
+ 
+    // delete a path 
+    // eg:  String HDFS_PATH = "hdfs://localhost:9000";
+    //      String FILE_PATH = "/user/output";
+	private static void deleteData(String HDFS_PATH, String FILE_PATH) throws IOException, URISyntaxException {
+        FileSystem fileSystem = FileSystem.get(new URI(HDFS_PATH), new Configuration());
+		fileSystem.delete(new Path(FILE_PATH), true);
+	}
+ 
+    // download data
+    // eg:  String HDFS_PATH = "hdfs://localhost:9000";
+    //      String FILE_PATH = "/user/output";
+	private static void downloadData(String HDFS_PATH, String FILE_PATH) throws IOException, URISyntaxException {
+        FileSystem fileSystem = FileSystem.get(new URI(HDFS_PATH), new Configuration());
+		FSDataInputStream in = fileSystem.open(new Path(FILE_PATH));
+		IOUtils.copyBytes(in, System.out, 1024, true);
+	}
+ 
+    // make a direction 
+    // eg:  String HDFS_PATH = "hdfs://localhost:9000";
+    //      String DIR_PATH = "/user";
+	private static void makeDir(String HDFS_PATH, String DIR_PATH) throws IOException, URISyntaxException {
+        FileSystem fileSystem = FileSystem.get(new URI(HDFS_PATH), new Configuration());
+		fileSystem.mkdirs(new Path(DIR_PATH));
+	}
+	
+    // upload data
+    // eg:  String HDFS_PATH = "hdfs://localhost:9000";
+    //      String FILE_PATH = "/user/output";
+    //      String LOCAL_PATH = "d:/log.txt";
+	private static void uploadData(String HDFS_PATH, String FILE_PATH, String LOCAL_PATH) throws IOException,
+			URISyntaxException {
+        FileSystem fileSystem = FileSystem.get(new URI(HDFS_PATH), new Configuration());
+		FSDataOutputStream out = fileSystem.create(new Path(FILE_PATH));
+		InputStream in = new FileInputStream(new File(LOCAL_PATH));
+		IOUtils.copyBytes(in, out, 1024, true);
+	}
+}
